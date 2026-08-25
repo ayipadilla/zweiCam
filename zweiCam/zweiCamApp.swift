33a996be601f3b7fe2a58b3db8a4a2adcb490d00
Danielle Padilla <danyelli@gmail.com>
2026-08-25 18:00:27 +0200

    Initial Commit

---
+++ zweiCam/zweiCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  zweiCamApp.swift
+//  zweiCam
+//
+//  Created by Ayi Padilla on 25.08.26.
+//
+
+import SwiftUI
+
+@main
+struct zweiCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
