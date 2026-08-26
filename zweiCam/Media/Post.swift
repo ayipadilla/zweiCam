2d6c9019e3697fcf85b3014c669325e26ce452e3
Danielle Padilla <danyelli@gmail.com>
2026-08-26 10:54:30 +0200

    Added Post media model

---
+++ zweiCam/Media/Post.swift
@@ -0,0 +1,22 @@
+//
+//  Post.swift
+//  zweiCam
+//
+//  Created by Ayi Padilla on 26.08.26.
+//
+
+import Foundation
+
+enum MediaType: String, Codable {
+    case photo
+    case video
+}
+
+struct Post: Codable, Identifiable {
+    let id: UUID
+    let createdAt: Date
+    let mediaType: MediaType
+    let backMediaPath: String
+    let frontMediaPath: String
+    let thumbnailPath: String
+}
